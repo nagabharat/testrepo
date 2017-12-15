7e5df0a179c4f025d87d51c05a07c01192c2498c
Your Name <you@example.com>
2017-12-15 21:23:29 +0530

    Initial application generated by JHipster-4.13.0

---
+++ src/main/java/com/synycs/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.synycs.config.audit;
---
+++ src/main/java/com/synycs/repository/PersistentTokenRepository.java
@@ -0,0 +1,19 @@
+package com.synycs.repository;
+
+import com.synycs.domain.PersistentToken;
+import com.synycs.domain.User;
+import java.time.LocalDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the PersistentToken entity.
+ */
+public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
